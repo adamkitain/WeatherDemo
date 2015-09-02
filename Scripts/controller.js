0a3834940753c186fc6265768f9a6afdf57cb145
Adam Kitain <adamkitain@gmail.com>
2015-09-02 14:16:46 -0400

    adding timeline

--- Scripts/controller.js
+++ Scripts/controller.js
@@ -8,7 +8,8 @@ angular.module('app', [])
 
       var mapWindow = {
         longleft: -110, longright: -80,
-        latupper: 41, latlower: 23
+        latupper: 41, latlower: 23,
+        levelofdetail: 0.1
       };
 
       $scope.kmlThreshs = {
@@ -17,26 +18,48 @@ angular.module('app', [])
 
       var kmls = {};
 
+      $scope.time = {
+        now: new Date(),
+        position: 0,
+        queryTime: null,
+        queryString: ""
+      };
+      $scope.time.queryTime = $scope.time.now;
+      $scope.time.queryString = $scope.time.queryTime.toISOString().substr(0,$scope.time.queryTime.toISOString().length - 5);
+
       $scope.map = new google.maps.Map(document.getElementById('map'), {
         center: {lat: 32.82, lng: -96.73},
-        zoom: 7
+        zoom: 11,
+        maxZoom: 14
       });
 
-      $scope.map.addListener('bounds_changed', setLayers);
+      $scope.map.addListener('idle', setLayers);
 
       function setLayers() {
-        console.log($scope.map.getBounds());
+        var bounds = $scope.map.getBounds();
+        mapWindow.longleft = bounds.Ea.j-0.5;
+        mapWindow.longright = bounds.Ea.G+0.5;
+        mapWindow.latupper = bounds.Ja.j+0.5;
+        mapWindow.latlower = bounds.Ja.G-0.5;
+        mapWindow.levelofdetail = Math.abs(mapWindow.longleft-mapWindow.longright)/800;
+        console.log(mapWindow.levelofdetail);
+
+        windLayer.url = getURL();
+        windLayer.setMap(null);
+        if($scope.kmlLayers.wind) {windLayer.setMap($scope.map) };
       }
 
+      
+
       var windLayer = new google.maps.KmlLayer({
-        url: "http://datacloud.wxc.com/?passkey=2a1f6d0b35ebb3bb0f100e3a05acd7ed&vs=1.0&datatype=forecast&format=kml&comparison=greaterthan&threshold="+$scope.kmlThreshs.wind+"&type=shape&lonleft=" + mapWindow.longleft + "&lonright=" + mapWindow.longright + "&latupper="+mapWindow.latupper+"&latlower="+mapWindow.latlower+"&var=WindSpeed_10m&time=now&polycolor=0:153:0:100&linecolor=50:200:50:0",
+        url: getURL(),
         map: null,
         preserveViewport: true
       });
 
-      $scope.changeLayerThresh = function (layer) {
+      $scope.drawLayer = function () {
         windLayer.setMap(null);
-        windLayer.url = "http://datacloud.wxc.com/?passkey=2a1f6d0b35ebb3bb0f100e3a05acd7ed&vs=1.0&datatype=forecast&format=kml&comparison=greaterthan&threshold="+$scope.kmlThreshs.wind+"&type=shape&lonleft=" + mapWindow.longleft + "&lonright=" + mapWindow.longright + "&latupper="+mapWindow.latupper+"&latlower="+mapWindow.latlower+"&var=WindSpeed_10m&time=now&polycolor=0:153:0:100&linecolor=50:200:50:0";
+        windLayer.url = getURL();
         if($scope.kmlLayers.wind) { windLayer.setMap($scope.map);}
       }
 
@@ -48,6 +71,17 @@ angular.module('app', [])
           windLayer.setMap(null);
         }
       };
+
+      $scope.updateTime = function () {
+        var addTime = $scope.time.position*60*1000;
+        $scope.time.queryTime.setTime($scope.time.now.getTime() + addTime);
+        $scope.time.queryString = $scope.time.queryTime.toISOString().substr(0,$scope.time.queryTime.toISOString().length - 5);
+        $scope.drawLayer();
+      }
+
+      function getURL() {
+        return "http://datacloud.wxc.com/?passkey=2a1f6d0b35ebb3bb0f100e3a05acd7ed&vs=1.0&datatype=forecast&format=kml&comparison=greaterthan&threshold="+$scope.kmlThreshs.wind+"&type=shape&lonleft=" + mapWindow.longleft + "&lonright=" + mapWindow.longright + "&latupper="+mapWindow.latupper+"&latlower="+mapWindow.latlower+"&var=WindSpeed_10m&time="+$scope.time.queryString+"&polycolor=0:153:0:100&linecolor=50:200:50:0&levelofdetail="+mapWindow.levelofdetail;
+      }
       
     }
   ])
