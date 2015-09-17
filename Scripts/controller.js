e7b04aa02073b6da4d6bb5719a418eae0803a39a
Daniel Shin <danieldshin@gmail.com>
2015-09-17 15:16:31 -0400

    Dan

--- Scripts/controller.js
+++ Scripts/controller.js
@@ -7,8 +7,8 @@ angular.module('app', [])
       };
 
       var mapWindow = {
-        longleft: -110, longright: -80,
-        latupper: 41, latlower: 23,
+        longleft: -94, longright: -60,
+        latupper: 47, latlower: 29,
         levelofdetail: 0.1
       };
 
@@ -37,12 +37,11 @@ angular.module('app', [])
 
       function setLayers() {
         var bounds = $scope.map.getBounds();
-        mapWindow.longleft = bounds.Ea.j-0.5;
-        mapWindow.longright = bounds.Ea.G+0.5;
-        mapWindow.latupper = bounds.Ja.j+0.5;
-        mapWindow.latlower = bounds.Ja.G-0.5;
+        mapWindow.longleft = bounds.Ga.j-0.5;
+        mapWindow.longright = bounds.Ga.H+0.5;
+        mapWindow.latupper = bounds.Ka.j+0.5;
+        mapWindow.latlower = bounds.Ka.H-0.5;
         mapWindow.levelofdetail = Math.abs(mapWindow.longleft-mapWindow.longright)/800;
-        console.log(mapWindow.levelofdetail);
 
         windLayer.url = getURL();
         windLayer.setMap(null);
@@ -82,6 +81,23 @@ angular.module('app', [])
       function getURL() {
         return "http://datacloud.wxc.com/?passkey=2a1f6d0b35ebb3bb0f100e3a05acd7ed&vs=1.0&datatype=forecast&format=kml&comparison=greaterthan&threshold="+$scope.kmlThreshs.wind+"&type=shape&lonleft=" + mapWindow.longleft + "&lonright=" + mapWindow.longright + "&latupper="+mapWindow.latupper+"&latlower="+mapWindow.latlower+"&var=WindSpeed_10m&time="+$scope.time.queryString+"&polycolor=0:153:0:100&linecolor=50:200:50:0&levelofdetail="+mapWindow.levelofdetail;
       }
+
+      $scope.streamPlay = function (weather) {
+	    console.log("success!");
+	    // SC.stream(stream_url, function(sound){
+	    //   $scope.weather = weather;
+	    //   // weather.play();
+	    // });
+
+	    $scope.isStreaming = true;
+	}
+
+		$scope.streamPause = function (weather) {
+	    // weather.pause()
+	    console.log("success!")
+
+	    $scope.isStreaming = false;
+	}
       
     }
   ])
